ba490f0d3d1c0cd2b952aab3346750c07cf28b49
duanzhenke <1830123756@qq.com>
2019-07-20 11:35:18 +0800

    第一次提交

---
+++ study001/src/main/java/com/huawei/service.java
@@ -0,0 +1,18 @@
+package com.huawei;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class service {
+
+    public static void main(String[] args) {
+       List<Integer> integers = new ArrayList<Integer>();
+
+       integers.add(233);
+        System.out.println(integers);
+
+
+
+
+    }
+}
